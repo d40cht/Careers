d2fde0a5bcb1d6e71ec49256faf29568fa5be0bd
Alex Wilson <alex_wilson@pobox.com>
2011-01-17 20:57:02 +0000

    Storing data.

--- apps/wikiprocess/main.cpp
+++ apps/wikiprocess/main.cpp
@@ -10,10 +10,14 @@ int main( int argc, char** argv )
     boost::scoped_ptr<ise::sql::DbResultSet> rs( db->select( "SELECT * FROM wordAssociation" ) );
     
     size_t count = 0;
+    std::map<size_t, std::pair<size_t, size_t> > wordAssocs;
 	while (true)
 	{
 		boost::tuple<int, int, double, double> t;
 		ise::sql::populateRowTuple( *rs, t );
+		
+		// Word id to (topic id, count)
+		wordAssocs[t.get<1>()].push_back( std::make_pair(t.get<0>(), t.get<2>()) );
 		
 		if ( ((++count) % 1000000) == 0 )
 		{
