a0ed9fd615278d1913fa5b03a2c40c98db6e44f6
AlexWilson <alex.wilson@ise-oxford.com>
2011-01-26 21:20:26 +0000

    Reporting.

--- fastcrunch.py
+++ fastcrunch.py
@@ -4,6 +4,7 @@ import bz2
 import math
 import string
 import sqlite3
+import datetime
 
 import cProfile
 
@@ -118,18 +119,22 @@ def run2():
     conn = sqlite3.connect( dbFileName, detect_types=sqlite3.PARSE_DECLTYPES )
     connOut = sqlite3.connect( dbOutFileName, detect_types=sqlite3.PARSE_DECLTYPES )
     
+    start = datetime.datetime.now()
     r = conn.execute( 'SELECT id, title, raw FROM rawTopics' )
     c = 0
-    for rowid, title, rawText in r:
+    for rowId, title, rawText in r:
         #text, templates = extractTemplates(rawText)
         #words = parsePage( text )
         text = extract.wikiStrip( rawText )
         connOut.execute( 'INSERT INTO topics VALUES(?, ?, ?)', (rowId, title, text) )
         c += 1
         if ((c % 1000) == 0):
-            print ':: %dk articles', c/1000
+            now = datetime.datetime.now()
+            hoursDiff = ((float) (now-start).seconds) / (3600.0)
+            print ':: %dk articles (%dk per hour)' % ((c/1000), ((float) c) / (1000.0 * hoursDiff)) 
 
         if ((c % 10000) == 0):
+            print 'Committing'
             connOut.commit()
 
 
