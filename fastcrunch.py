540e7fd48569435413c41d5bfc32568bc5e7f448
Alex Wilson <alex_wilson@pobox.com>
2011-01-26 21:11:15 +0000

    Stripped data into output db.

--- fastcrunch.py
+++ fastcrunch.py
@@ -114,7 +114,9 @@ if 0:
 
 def run2():
     dbFileName = 'rawData.sqlite3'
+    dbOutFileName = 'text.sqlite3'
     conn = sqlite3.connect( dbFileName, detect_types=sqlite3.PARSE_DECLTYPES )
+    connOut = sqlite3.connect( dbOutFileName, detect_types=sqlite3.PARSE_DECLTYPES )
     
     r = conn.execute( 'SELECT id, title, raw FROM rawTopics' )
     c = 0
@@ -122,8 +124,13 @@ def run2():
         #text, templates = extractTemplates(rawText)
         #words = parsePage( text )
         text = extract.wikiStrip( rawText )
+        connOut.execute( 'INSERT INTO topics VALUES(?, ?, ?)', (rowId, title, text) )
         c += 1
-        if ((c % 100) == 0): print ':: ', c
+        if ((c % 1000) == 0):
+            print ':: %dk articles', c/1000
+
+        if ((c % 10000) == 0):
+            connOut.commit()
 
 
 if __name__ == '__main__':
