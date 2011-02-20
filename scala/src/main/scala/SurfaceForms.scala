c52c3e8b908a7f0697ea815a4128c6b7d982cdb3
Alex Wilson <alex_wilson@pobox.com>
2011-02-20 22:10:29 +0000

    Plumbed dbpedia parsing in to first mapreduce job.

--- scala/src/main/scala/SurfaceForms.scala
+++ scala/src/main/scala/SurfaceForms.scala
@@ -10,11 +10,89 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.hadoop.util.GenericOptionsParser
 import scala.collection.JavaConversions._
 
+import org.dbpedia.extraction.wikiparser._
+import org.dbpedia.extraction.sources.WikiPage
+import org.dbpedia.extraction.wikiparser.{Node}
+
+
 class SurfaceFormsMapper extends Mapper[Text, Text, Text, Text]
 {
-    override def map(key : Text, value : Text, context : Mapper[Text, Text, Text, Text]#Context) =
+    val markupParser = WikiParser()
+    
+    def extractLinks( elements : Seq[Node], context : Mapper[Text, Text, Text, Text]#Context )
     {
-        //context.write( surfaceForm, topic )
+        for ( child <- elements )
+        {
+            child match
+            {
+                case InternalLinkNode(destination, children, line) =>
+                {
+                    // Interested in 'Main' or 'Category' largely
+                    //println( "    " + destination.namespace + ", "  + destination.decoded)
+                    
+                    if ( children.length != 0 )
+                    {
+                        val destinationTopic = destination.decoded
+                        
+                        children(0) match
+                        {
+                            case TextNode( surfaceForm, line ) =>
+                            {
+                                context.write( new Text(surfaceForm), new Text(destinationTopic) )
+                            }
+                            case _ => throw new ClassCastException()
+                        }
+                    }
+                }
+
+                case SectionNode(name, level, children, line ) =>
+                {
+                    //println( "  Section: " + name )
+                    extractLinks( children, context )
+                }
+
+                case TemplateNode( title, children, line ) =>
+                {
+                    // Namespace is 'Template'. Don't want POV, advert, trivia
+                    //println( "  " + title.namespace + ", " + title.decoded )
+                    extractLinks( children, context )
+                }
+
+                case TableNode( caption, children, line ) =>
+                {
+                    extractLinks( children, context )
+                }
+
+                case TextNode( text, line ) =>
+                {
+                    // Nothing for now
+                }
+
+                case _ =>
+            }
+        }
+    }
+
+                
+    override def map( key : Text, value : Text, context : Mapper[Text, Text, Text, Text]#Context ) =
+    {
+        val topicTitle = key
+        val topicText = value
+        
+        val page = new WikiPage( WikiTitle.parse( topicTitle.toString ), 0, 0, topicText.toString )
+        try
+        {
+            val parsed = markupParser( page )
+            extractLinks( parsed.children, context )
+            //context.write( surfaceForm, topic )
+        }
+        catch
+        {
+            case e : WikiParserException =>
+            {
+                println( "Bad parse: " + topicTitle )
+            }
+        }
     }
 }
 
