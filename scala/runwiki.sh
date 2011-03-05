50ebd3d711b533eb178f1793990420e8dd6b1786
Alex Wilson <alex_wilson@pobox.com>
2011-03-05 13:42:42 +0000

    Added category membership parsing.

--- scala/runwiki.sh
+++ scala/runwiki.sh
@@ -1,1 +1,2 @@
-/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar SurfaceForms enwikifull.seq enwikires
+/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar SurfaceForms enwikifull.seq surfaceformres
+/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar CategoryMembership enwikifull.seq categoryres
