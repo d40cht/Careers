0b5621650688b68534187fce21044b1e4cc24f1a
AlexWilson <alex.wilson@ise-oxford.com>
2011-03-08 15:31:07 +0000

    Somewhat working. Horrible memory footprint.

--- scala/src/main/scala/PhraseMap.scala
+++ scala/src/main/scala/PhraseMap.scala
@@ -1,7 +1,12 @@
+import java.io.{BufferedReader, InputStreamReader}
+
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.conf.Configuration
 
 import scala.collection.immutable.TreeMap
+
+import edu.umd.cloud9.io.JSONObjectWritable
+import org.json.JSONException
 
 class PhraseNode[TargetType]
 {
@@ -103,9 +108,20 @@ class PhraseWalker[TargetType]( val phraseMap : PhraseMap[TargetType], val phras
     }
 }
 
+// Helper to allow iterating over java util iterators
+class IteratorWrapper[A](iter:java.util.Iterator[A])
+{
+    def foreach(f: A => Unit): Unit = {
+        while(iter.hasNext){
+          f(iter.next)
+        }
+    }
+}
 
 object PhraseMap
 {
+    implicit def iteratorToWrapper[T](iter:java.util.Iterator[T]):IteratorWrapper[T] = new IteratorWrapper[T](iter)
+     
     def isNonWordChar( el : Char ) : Boolean =
     {
         el match
@@ -120,12 +136,98 @@ object PhraseMap
             case _ => return false
         }
     }
+    
+    
+    private def parseFile( pm : PhraseMap[String], fs : FileSystem, path : Path )
+    {
+        assert( fs.exists(path) )
+        val reader = new BufferedReader( new InputStreamReader( fs.open( path ) ) )
+        
+        var count = 0
+        var line : String = null
+        while ( {line = reader.readLine(); line != null} )
+        {
+            val firstTabIndex = line.indexOf( '\t' )
+            val surfaceForm = line.substring(0,firstTabIndex)
+            val references = line.substring(firstTabIndex+1)
+            
+            try
+            {
+                val parsed = new JSONObjectWritable( references )
+                
+                var kk = 0
+                for ( key <- parsed.keys() )
+                {
+                    pm.addPhrase( surfaceForm, parsed.getString(key) )
+                    kk += 1
+                }
+                
+                if ( kk > 40 )
+                {
+                    //println( count + " == " + line )
+                }
+                count += 1
+            }
+            catch
+            {
+                case e : org.json.JSONException =>
+                {
+                    println( "JSON exception parsing: " + line )
+                }
+                case e : Exception =>
+                {
+                    println( "General exception: " + e.toString )
+                }
+            }
+        }
+        reader.close()
+    }
 
     def main( args : Array[String] )
     {
-        val path = new Path("hdfs://shinigami.lan.ise-oxford.com:54310/user/alexw/surfaceformres")
         val conf = new Configuration()
-        val fs = FileSystem.get(conf)
+        conf.addResource(new Path("/home/hadoop/hadoop/conf/core-site.xml"));
+        conf.addResource(new Path("/home/hadoop/hadoop/conf/hdfs-site.xml"));
+        val fs = FileSystem.get(conf)   
+        
+        
+        val pm = new PhraseMap[String]()
+        
+        val path0 = new Path("hdfs://shinigami.lan.ise-oxford.com:54310/user/alexw/surfaceformres/part-r-00000")
+        val path1 = new Path("hdfs://shinigami.lan.ise-oxford.com:54310/user/alexw/surfaceformres/part-r-00001")
+        val path2 = new Path("hdfs://shinigami.lan.ise-oxford.com:54310/user/alexw/surfaceformres/part-r-00002")
+        val path3 = new Path("hdfs://shinigami.lan.ise-oxford.com:54310/user/alexw/surfaceformres/part-r-00003")
+        val path4 = new Path("hdfs://shinigami.lan.ise-oxford.com:54310/user/alexw/surfaceformres/part-r-00004")
+        
+        
+        parseFile( pm, fs, path0 )
+        parseFile( pm, fs, path1 )
+        parseFile( pm, fs, path2 )
+        parseFile( pm, fs, path3 )
+        parseFile( pm, fs, path4 )
+        
+        def printRes( m : String, terminals : List[String] )
+        {
+            println( "Found: " + m + ", " + terminals.toString() )
+        }
+        
+        while ( true )
+        {
+            val query = Console.readLine
+            val pw = new PhraseWalker( pm, printRes )
+            var lastChar = ' '
+            for (c <- query )
+            {
+                if ( PhraseMap.isNonWordChar(lastChar) )
+                {
+                    pw.startNew()
+                    
+                }
+                pw.update( c )
+                lastChar = c
+            }
+            pw.update( ' ' )
+        }
     }
 }
 
