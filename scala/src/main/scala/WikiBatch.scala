eb6990f71590e5f69ba2c42e33c30353ca3452c4
Alex Wilson <alex_wilson@pobox.com>
2011-03-27 20:36:47 +0100

    Added missing file.

---
+++ scala/src/main/scala/WikiBatch.scala
@@ -0,0 +1,29 @@
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.util.GenericOptionsParser
+
+// Run: Category membership, Surface forms, Word in document count.
+
+// To add: link counts - forward and backwards.
+// Additionally include contexts ala microsoft research paper.
+
+object WikiBatch
+{
+    def main(args:Array[String]) : Unit =
+    {
+        // Run Hadoop jobs
+        val conf = new Configuration()
+        val otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs
+        
+        val inputFile = args(0)
+        val outputPathBase = args(1)
+        val outputDbFileName = args(2)
+        val numReduces = args(3).toInt
+        
+        CategoryMembership.run( conf, inputFile, outputPathBase + "/categoryMembership", numReduces )
+        SurfaceForms.run( conf, inputFile, outputPathBase + "/surfaceforms", numReduces)
+        WordInDocumentMembership.run( conf, inputFile, outputPathBase + "/wordInDocumentCount", numReduces)
+        
+        // Now pull them all in and build the sqlite db
+        
+    }
+}
