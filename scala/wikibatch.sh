97f28fea8c01fbcbfb131fece8cf65113fcfafaa
AlexWilson <alex.wilson@ise-oxford.com>
2011-04-05 20:45:22 +0100

    Refactoring of phrase builder.

--- scala/wikibatch.sh
+++ scala/wikibatch.sh
@@ -1,2 +1,2 @@
-/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar WikiBatch enwikifull.seq wikiBatch 10
+/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar WikiBatch enwikismall.seq wikiBatch 10
 #java -cp target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar PhraseMap wikiBatch/ dbout.sqlite
