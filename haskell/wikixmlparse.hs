95e256c2bd9c56a32552e36ddfd822ce7ccff877
Alex Wilson <alex_wilson@pobox.com>
2011-04-25 22:07:49 +0100

    Bugger me. Got my instance of Binary working! Hurrah for starting to understand the Haskell type system.

--- haskell/wikixmlparse.hs
+++ haskell/wikixmlparse.hs
@@ -101,24 +101,33 @@ chunkList list chunkSize =
 incrementalPut list chunkSize =
     mapM_ (\x -> put (fst x) >> put (snd x)) $ chunkList list chunkSize
 
+{-
+-- The type signature below means that for a Binary serializable object 'a' ('Binary a =>'),
+-- this instance will serialize a list of [a] (Binary [a])
+
+instance Binary a => Binary [a] where
+    put l  = put (length l) >> mapM_ put l
+    get    = do n <- get :: Get Int
+                replicateM n get
+-}
+
+getLazySerializingList :: Binary a => Get [a]
+getLazySerializingList = do
+    length <- get :: Get Int
+    headChunk <- get
+    tailChunk <- if (length==0) then do
+        return []
+        else do
+            tailData <- getLazySerializingList
+            return tailData
+    return $ headChunk ++ tailChunk
+
 instance (Binary a) => Binary (LazySerializingList a) where
     put (LazySerializingList l) = incrementalPut l 8
     get = do
-        --resValue <- incrementalGet
-        length <- get :: Get Int
-        headChunk <- replicateM length get
-        tailChunk <- if (length==0) then do
-                return []
-                else do
-                    value <- get
-                    return value
-        return $ LazySerializingList $ headChunk ++ tailChunk
+        listData <- getLazySerializingList
+        return $ LazySerializingList listData
 
-
-
---blah = LazySerializingList [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
-
-blah2 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 blah = LazySerializingList ["1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -126,7 +135,6 @@ blah = LazySerializingList ["1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2
 main = do
     -- Quick check of encode functionality
     let res = encode blah
-    print res
     let res2 = decode res
     print (blah == res2)
 
