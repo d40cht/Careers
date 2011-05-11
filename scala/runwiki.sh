4c4ca1eefeadc77606f44e09f520acfbc5a81531
AlexWilson <alex.wilson@ise-oxford.com>
2011-05-11 17:54:19 +0100

    Additional script.

--- scala/runwiki.sh
+++
@@ -1,2 +0,0 @@
-/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar SurfaceForms enwikifull.seq surfaceformres 30
-/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar CategoryMembership enwikifull.seq categoryres 30
---
+++ scala/wikibatchsmall.sh
@@ -0,0 +1,4 @@
+
+export LD_LIBRARY_PATH=/home/alexw/AW/optimal/scala/lib
+/home/hadoop/hadoop/bin/hadoop jar ./target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar WikiBatch enwikismall.seq wikiBatchSmall 10
+#java -cp target/scala_2.8.1/wikicrunch_2.8.1-1.0.min.jar PhraseMap wikiBatchSmall/ dbout.sqlite
