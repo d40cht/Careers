918e7ffd52b9767cc44d2466986c3a593460f9df
Alex Wilson <alex_wilson@pobox.com>
2011-06-24 21:50:46 +0100

    Some fake user management logic. All quite nice really.

--- play/optimal/app/controllers.scala
+++ play/optimal/app/controllers.scala
@@ -7,8 +7,35 @@ object Application extends Controller {
     
     import views.Application._
     
-    def index = html.index()
+    def index = html.index( session, flash )
+    def login =
+    {
+        val name = params.get("username")
+
+        if ( name == null )
+        {
+            html.login( session, flash )
+        }        
+        else if ( name == "alex" )
+        {
+            flash += ("info" -> ("Welcome " + name))
+            session += ("user" -> "alex")
+            Action(index)
+        }
+        else
+        {
+            flash += ("error" -> ("Unknown user " + name))
+            html.login(session, flash)
+        }
+    }
+    def logout =
+    {
+        val name = session.get("user")
+        session.remove("user")
+        flash += ("info" -> ("Goodbye: " + name) )
+        Action(index)
+    }
     
-    def sayHello = html.sayHello(params.get("myName"))
-    
+    def register = html.register( session, flash )
+       
 }
