10f43c69a88c9ea321ee92220e7f4636217b9766
Alex Wilson <alex_wilson@pobox.com>
2011-08-31 22:37:06 +0100

    Limit processing to 2000 words for efficiency reasons. Can be lifted later.

--- scala/src/main/scala/libraries/disambiguator/Disambiguator.scala
+++ scala/src/main/scala/libraries/disambiguator/Disambiguator.scala
@@ -396,7 +396,7 @@ class Disambiguator( phraseMapFileName : String, topicFileName : String, categor
             var topicSet = TreeSet[Int]()
             
             logger.info( "Parsing text and building topic and category maps." )
-            for ( word <- wordList )
+            for ( word <- wordList.slice(0, 2000) )
             {
                 //logger.info( "Reading: " + word )
                 val wordLookup = lookup.lookupWord( word )
