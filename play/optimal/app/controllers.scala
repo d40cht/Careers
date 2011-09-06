196cc0074702f968fcd05cd7a5cf3d4d3680bfdc
AlexWilson <alex.wilson@ise-oxford.com>
2011-09-06 18:16:52 +0100

    Munge.

--- play/optimal/app/controllers.scala
+++ play/optimal/app/controllers.scala
@@ -193,7 +193,7 @@ object Application extends Controller {
             
             <cvs>
             {
-                for ( (id, dd) <- unprocessedCVs.list.filter( _._2 ) ) yield <id>{id}</id>
+                for ( (id ~ dd) <-unprocessedCVs ) yield <id>{id}</id><dd>{dd.toString}</dd>
             }
             </cvs>
         }
